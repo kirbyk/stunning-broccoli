e0b391c72d9ac075e4c6313642299de4c984aeb6
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2016-02-02 15:58:01 -0500

    Add basic black block moving around implementation

--- server/assets/js/main.js
+++ server/assets/js/main.js
@@ -6,6 +6,11 @@ var height = 600;
 
 var heroWidth = 20;
 var heroHeight = 20;
+var heroJump = -300;
+var heroSpeed = 250;
+
+var rightKeyDown = false;
+var leftKeyDown = false;
 
 var centerX = width / 2;
 var centerY = height / 2;
@@ -15,6 +20,14 @@ var fps = 1000 / 60; // 60 fps
 
 var GRAVITY = 300;
 var lastTime = null;
+
+var keyCodes = {
+  space: 32,
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40
+};
 
 var Player = function() {
   this.x = centerX;
@@ -35,10 +48,47 @@ var init = function() {
 
   document.body.appendChild(canvas);
 
+  document.addEventListener('keydown', handleKeydown);
+  document.addEventListener('keyup', handleKeyup);
+
   hero = new Player();
   lastTime = Date.now();
 
   gameLoopInterval = setInterval(gameLoop, fps);
+};
+
+var handleKeydown = function(e) {
+  var keyCode = e.keyCode;
+
+  console.log(keyCode);
+
+  switch(keyCode) {
+    case keyCodes.up:
+    case keyCodes.space:
+      hero.yDir = heroJump;
+      break;
+    case keyCodes.right:
+      rightKeyDown = true;
+      break;
+    case keyCodes.left:
+      leftKeyDown = true;
+      break;
+  }
+};
+
+var handleKeyup = function(e) {
+  var keyCode = e.keyCode;
+
+  console.log(keyCode);
+
+  switch(keyCode) {
+    case keyCodes.right:
+      rightKeyDown = false;
+      break;
+    case keyCodes.left:
+      leftKeyDown = false;
+      break;
+  }
 };
 
 var draw = function() {
@@ -50,14 +100,28 @@ var draw = function() {
 var update = function() {
   var deltaTime = (Date.now() - lastTime) / 1000;
 
-  console.log(deltaTime);
-
   hero.yDir += GRAVITY * deltaTime;
 
   if (hero.y + hero.yDir * deltaTime + hero.height < height) {
     hero.y += hero.yDir * deltaTime;
   } else {
     hero.y = height - hero.height;
+  }
+
+  if (rightKeyDown) {
+    if (hero.x + heroSpeed * deltaTime + hero.width < width) {
+      hero.x += heroSpeed * deltaTime;
+    } else {
+      hero.x = width - hero.width;
+    }
+  }
+
+  if (leftKeyDown) {
+    if (hero.x - heroSpeed * deltaTime > 0) {
+      hero.x -= heroSpeed * deltaTime;
+    } else {
+      hero.x = 0;
+    }
   }
 
   lastTime = Date.now();
@@ -67,6 +131,7 @@ var gameLoop = function() {
   draw();
   update();
 };
+
 
 
 init();
